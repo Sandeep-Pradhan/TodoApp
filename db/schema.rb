a143217dba44005c5559544db094b8ac00189cee
Sandeep <sandeeppradhan313@gmail.com>
2020-05-18 19:31:02 +0530

    Added the Priority Feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_120938) do
+ActiveRecord::Schema.define(version: 2020_05_18_135205) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_18_120938) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "deadline"
+    t.integer "priority"
   end
 
 end
